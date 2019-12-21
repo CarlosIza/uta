58af51224918a51ddc1279e140c9b5e6cd3d9a52
CarlosIza <46698062+CarlosIza@users.noreply.github.com>
2019-12-21 18:26:21 -0500

    completo

--- principal/principal/Program.cs
+++ principal/principal/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pruebaCarlosDos
+namespace principal
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("hola munfo");
-			Console.WriteLine("commit dos");
 		}
 	}
 }
