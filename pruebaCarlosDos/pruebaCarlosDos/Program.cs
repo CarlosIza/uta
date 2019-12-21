162127eb43fdeac04aaf986f4e12105fcdbb1ff0
CarlosIza <46698062+CarlosIza@users.noreply.github.com>
2019-12-21 18:23:54 -0500

    clase1

---
+++ pruebaCarlosDos/pruebaCarlosDos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaCarlosDos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("hola munfo");
+		}
+	}
+}
